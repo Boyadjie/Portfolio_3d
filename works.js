7a017f06bc204028a6736d89517662e5f7d1d7ad
pch-emeric <emeric.boyadjian@pch.click>
2022-10-25 12:16:00 +0200

    complete works page and contact

---
+++ works.js
@@ -0,0 +1,117 @@
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
+
+const projects = document.querySelectorAll("section");
+
+//  Show-Hide details
+projects.forEach((project) => {
+  const title = project.querySelector("h2");
+  const details = project.querySelector(".project-details");
+  const toShow = project.querySelectorAll(".toShow");
+  const closeBtn = project.querySelector(".closeDetails");
+  let delay;
+
+  title.addEventListener("click", () => {
+    delay = 1;
+    gsap.to(details, {
+      duration: 1,
+      display: "block",
+      height: 443,
+      padding: 20,
+    });
+
+    for (let i = 0; i < toShow.length; i++) {
+      const element = toShow[i];
+
+      gsap.to(element, {
+        duration: 0.5,
+        delay,
+        display: "block",
+        opacity: 1,
+        y: 0,
+      });
+
+      delay += 0.1;
+    }
+  });
+
+  closeBtn.addEventListener("click", () => {
+    delay = 0.3;
+
+    for (let i = toShow.length; i >= 0; i--) {
+      const element = toShow[i];
+
+      gsap.to(element, {
+        duration: 0.5,
+        display: "none",
+        opacity: 0,
+        y: -100,
+      });
+    }
+
+    gsap.to(details, {
+      duration: 1,
+      display: "none",
+      delay,
+      height: 0,
+      padding: 0,
+    });
+  });
+});
+
+gsap.to("#first", {
+  x: 80,
+  scrollTrigger: {
+    trigger: "section:nth-child(1)",
+    start: "top bottom",
+    end: "bottom top",
+    scrub: 0.5,
+  },
+});
+
+gsap.to("#second", {
+  x: -80,
+  scrollTrigger: {
+    trigger: "section:nth-child(2)",
+    start: "top bottom",
+    end: "bottom top",
+    scrub: 0.5,
+  },
+});
+gsap.to("#third", {
+  x: 80,
+  scrollTrigger: {
+    trigger: "section:nth-child(3)",
+    start: "top bottom",
+    end: "bottom top",
+    scrub: 0.5,
+  },
+});
+gsap.to("#fourth", {
+  x: -80,
+  scrollTrigger: {
+    trigger: "section:nth-child(4)",
+    start: "top bottom",
+    end: "bottom top",
+    scrub: 0.5,
+  },
+});
+gsap.to("#fifth", {
+  x: 80,
+  scrollTrigger: {
+    trigger: "section:nth-child(5)",
+    start: "top bottom",
+    end: "bottom top",
+    scrub: 0.5,
+  },
+});
+gsap.to("#sixth", {
+  x: -80,
+  scrollTrigger: {
+    trigger: "section:nth-child(6)",
+    start: "top bottom",
+    end: "bottom top",
+    scrub: 0.5,
+  },
+});
